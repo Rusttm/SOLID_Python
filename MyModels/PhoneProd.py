5fea2bdf06a4aa76c665c50797c9b75d57977822
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-04-29 22:14:38 +0300

    product fin commit

--- MyModels/PhoneProd.py
+++ MyModels/PhoneProd.py
@@ -5,14 +5,21 @@ from MyModels.ProdMainClass import ProdMainClass
 class PhoneProd(ProdMainClass):
     id = 0
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__()
-        self.id = super().getProdID()
-        print(f"{self.id=}")
+        PhoneProd.id = super().get_prod_id()
+        self.prod = dict()
+        self.prod.update(kwargs)
+        self.prod.update(id=PhoneProd.id)
 
+    def get_prod_dict(self):
+        return self.prod
 
 
 if __name__ == "__main__":
-    newClassObj = PhoneProd()
-    newClassObj2 = PhoneProd()
-    newClassObj3 = PhoneProd()
+    newPhone1 = PhoneProd(name="iPhone", model="SE", brand="Apple", year="2015", memory="64", descr="refurbished", price="52000")
+    newPhone2 = PhoneProd(name="iPhone", model="13 Pro Max", brand="Apple", year="2020", memory="256", descr="new", price="125000")
+    newPhone3 = PhoneProd(name="Redmi", model="MI9", brand="Xiaomi", year="2018", memory="64", descr="", price="11000")
+
+    print(newPhone3.get_prod_id())
+    print(newPhone2.get_prod_dict())
